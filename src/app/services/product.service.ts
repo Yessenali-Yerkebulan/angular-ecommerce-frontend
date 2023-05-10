123307e91c0c73bbcd9b5afdf123095e9ba265d3
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-10 22:15:23 +0600

    product.service.ts confugration

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl='http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse{
+  _embedded: {
+    products: Product[];
+  }
 }
