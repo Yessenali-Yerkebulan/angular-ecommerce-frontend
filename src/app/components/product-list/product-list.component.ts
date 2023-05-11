c227a35c90d3c8aa05f38e7056ab15870053ea80
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-11 19:31:36 +0600

    show products in HTML table and add product images

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,8 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  // templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
