12f066f2b57a0193df681107b7557aa1a312bdba
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-11 19:06:10 +0600

    update angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClientModule} from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  products: Product[] = [];
+  constructor(private productService: ProductService){}
 
+  ngOnInit(): void{
+    this.listProducts();
+  }
+
+  listProducts(){
+    this.productService.getProductList().subscribe(
+      data=>{
+        this.products = data;
+      }
+    )
+  }
 }
