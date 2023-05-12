cdc2adbb06ceccff0cfc94874f902f92d7f2b9cf
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-12 19:30:57 +0600

    add support for Product Grid

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,8 +4,7 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list.component.html',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
