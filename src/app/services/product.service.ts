f62d62d9c7a748a9502fa5c8c40217f9a6f138ae
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-16 19:43:25 +0600

    add searchUrl

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,7 +14,10 @@ export class ProductService {
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
