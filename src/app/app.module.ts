23446a666444326d5ff1ce4d2095d0efae6a85af
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-17 11:23:29 +0600

    display category name in product list grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {Routes, RouterModule} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 
 const routes: Routes = [
-  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category/:id/:name', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
   {path: '', redirectTo: '/products', pathMatch: 'full'},
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,44 +1,52 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Product } from 'src/app/common/product';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
   products: Product[] = [];
   currentCategoryId: number = 1;
-  constructor(private productService: ProductService,
-      private route: ActivatedRoute){}
+  currentCategoryName: string = "";
 
-  ngOnInit(): void{
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
   }
 
-  listProducts(){
+  listProducts() {
 
-    // check if "id" parameter is avaiable
+    // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-    if(hasCategoryId){
-      // get the  "id" param string. convert string to a number using the "+" symbol
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-    }else{
-      // not category id available ... default to category id 1
-      this.currentCategoryId=1;
-    }
 
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
+    }
+    else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
+    }
 
     // now get the products for the given category id
     this.productService.getProductList(this.currentCategoryId).subscribe(
-      data=>{
+      data => {
         this.products = data;
       }
     )
   }
+
 }
