b9303f2f8c9083c7bab9818130ff2914041e4933
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-20 18:50:43 +0600

    add searchProducts method with url and refactor code by creating getProducts method

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -20,6 +20,16 @@ export class ProductService {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
