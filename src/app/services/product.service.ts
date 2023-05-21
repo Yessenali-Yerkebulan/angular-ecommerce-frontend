2e322cbb64890349206a2680a5b51d388fee46db
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-21 20:56:58 +0600

    refactor the interface for GetResponseProducts and add pagination support to ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,6 +22,17 @@ export class ProductService {
     return this.httpClient.get<Product>(productUrl);
   }
 
+  getProductListPaginate(thePage: number,
+     thePageSize: number,
+      theCategoryId: number): Observable<GetResponseProducts> {
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+    +`&page=${thePage}&size=${thePage};
+    `;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]> {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
@@ -51,6 +62,12 @@ export class ProductService {
 interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
