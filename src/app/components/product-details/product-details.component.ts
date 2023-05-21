57a1e2328680228cb2182479244a69eb0e403e76
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-21 16:07:40 +0600

    enhance ProductDetailsComponent to retrieve product from ProductService

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
+  product!: Product;
 
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute){
+
+    }
+
+    ngOnInit(): void{
+      this.route.paramMap.subscribe(() =>{
+        this.handleProductDetails();
+      })
+    }
+  handleProductDetails() {
+
+    // get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.productService.getProduct(theProductId).subscribe(
+      data=>{
+        this.product=data;
+      }
+    )
+  }
 }
