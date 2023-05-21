e22e686bdf91172b3c8db3a1ae17c3562089b03d
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-21 21:12:00 +0600

    update ProductListComponent to handle pagination

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,8 +12,15 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   currentCategoryName: string = "";
   searchMode: boolean = false;
+
+  //new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
+
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -62,11 +69,31 @@ export class ProductListComponent implements OnInit {
          this.currentCategoryName = 'Books';
        }
 
+       // Check if we have a different category than previous
+       // Note: Angular will reuse a component if it is currently being viewed
+
+       // if we have a different category id than previous
+       // then set thePageNumber back to 1
+       if(this.previousCategoryId != this.currentCategoryId){
+        this.thePageNumber=1;
+       }
+
+       this.previousCategoryId = this.currentCategoryId;
+
+       console.log(`currentCategoryId=${this.currentCategoryId}, this.thePageNumber=${this.thePageNumber}`);
+
        // now get the products for the given category id
-       this.productService.getProductList(this.currentCategoryId).subscribe(
-         data => {
-           this.products = data;
-         }
-       )
+       this.productService.getProductListPaginate(
+        this.thePageNumber - 1,
+        this.thePageSize,
+        this.currentCategoryId
+       ).subscribe(
+          data => {
+            this.products = data._embedded.products;
+            this.thePageNumber = data.page.number + 1;
+            this.thePageSize = data.page.size;
+            this.theTotalElements = data.page.totalElements;
+          }
+        );
   }
 }
