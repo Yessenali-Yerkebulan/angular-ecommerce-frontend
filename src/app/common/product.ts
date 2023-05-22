d93a6b2932dcad1be82aec9c392f76a34b132a5f
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-22 08:24:08 +0600

    create model class: CartItem

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,6 +1,6 @@
 export class Product {
     constructor(
-        public id: number,
+        public id: string,
         public sku: string,
         public name: string,
         public description: string,
