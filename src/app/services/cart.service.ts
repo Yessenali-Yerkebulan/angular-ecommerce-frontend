81564ce37144a4c1b6961b72e304f8ad8cba2918
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-22 09:18:46 +0600

    refactor cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -21,12 +21,7 @@ export class CartService {
     if(this.cartItems.length>0){
       // find the item in the cart based on item id
 
-      for(let tempCartItem of this.cartItems){
-        if(tempCartItem.id == theCartItem.id){
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id );
 
       // check if we found it
     alreadyExistsInCart = (existingCartItem != undefined);
