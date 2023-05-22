7818e5045d428e7b823718471664ee0d6a7f76a3
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-22 18:59:48 +0600

    repeat process for remove button

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -42,4 +42,8 @@ export class CartDetailsComponent {
   decrementQuantity(theCartitem: CartItem){
     this.cartService.decrementQuantity(theCartitem);
   }
+
+  remove(theCartItem: CartItem){
+    this.cartService.remove(theCartItem);
+  }
 }
