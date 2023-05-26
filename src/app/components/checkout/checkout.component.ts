1963f34987929a046fe5a9861f9f693691acb740
Yerkebulan <yerkebulanyessenali03@gmail.com>
2023-05-26 22:01:44 +0600

    add whitespace validation rule for the form controls(first_name, last_name, email) and update HTML template to display error messages

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { ShopFormService } from 'src/app/services/shop-form.service';
+import { ShopValidators } from 'src/app/validators/shop-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -28,10 +29,11 @@ export class CheckoutComponent {
   ngOnInit(): void{
       this.checkoutFormGroup = this.formBuilder.group({
         customer: this.formBuilder.group({
-          firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-          lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+          firstName: new FormControl('',
+           [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
+          lastName: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
           email: new FormControl('',
-            [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
+            [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'), ShopValidators.notOnlyWhitespace]
           )
         }),
         shippingAddress: this.formBuilder.group({
