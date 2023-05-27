b4f0deacef82e01e7b3c27d2e09997df234583ba
Yerkebulan <yerkebulanyessenali03@gmail.com>
2023-05-27 19:45:07 +0600

    add whitespace validation rule for the credit card form and update HTML template to display error messages, fix bugs

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -51,10 +51,10 @@ export class CheckoutComponent {
           zipCode: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace])
         }),
         creditCard: this.formBuilder.group({
-          cardType: [''],
-          nameOnCard: [''],
-          cardNumber: [''],
-          securityCode: [''],
+          cardType: new FormControl('', [Validators.required]),
+          nameOnCard: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
+          cardNumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16}')]),
+          securityCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')]),
           expirationMonth: [''],
           expirationYear: ['']
         })
@@ -121,6 +121,7 @@ export class CheckoutComponent {
     return this.checkoutFormGroup.get('shippingAddress.country');
   }
 
+
   get billingAddressStreet(){
     return this.checkoutFormGroup.get('billingAddress.street');
   }
@@ -137,6 +138,19 @@ export class CheckoutComponent {
     return this.checkoutFormGroup.get('billingAddress.country');
   }
 
+
+  get creditCardType(){
+    return this.checkoutFormGroup.get('creditCard.cardType');
+  }
+  get creditCardNameOnCard(){
+    return this.checkoutFormGroup.get('creditCard.nameOnCard');
+  }
+  get creditCardNumber(){
+    return this.checkoutFormGroup.get('creditCard.cardNumber');
+  }
+  get creditCardSecurityCode(){
+    return this.checkoutFormGroup.get('creditCard.securityCode');
+  }
 
   copyShippingAddressToBillingAddress(event){
     if(event.target.checked){
